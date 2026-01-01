1026958de56aedee30659e47e45593d6b69a3b59
Emmanuel Ndeh <emmanuelrigobert000@gmail.com>
2026-01-01 20:05:20 +0100

    Implement arrayCopy method and enhance appendString logic in ArrayStringList; update countElements to return correct value.

--- src/miniProject3/ArrayStringList.java
+++ src/miniProject3/ArrayStringList.java
@@ -1,4 +1,7 @@
 package miniProject3;
+
+import java.util.Arrays;
+
 /**
  * An array list to store strings.
  * 
@@ -18,9 +21,34 @@ public class ArrayStringList implements StringList {
 		super();
 	}
 
+	public  <T> void arrayCopy(T[] source, T[] destination, int startIndex, int stopIndex){
+		for(int i=startIndex; i<= stopIndex; i++){
+			destination[i]=source[i];
+		}
+	}
+
 	@Override
 	public void appendString(String text) {
 		// TODO: implement
+		boolean appended =  addElt(items, text);
+
+		if(!appended){
+			String[] newItems = new String[items.length+1];
+			arrayCopy( items, newItems,0, items.length-1);
+			items=newItems;
+			addElt(items,text);
+			System.out.println(Arrays.toString(items));
+		}
+
+	}
+	public boolean addElt(String[] items,String text){
+		for(int i=0; i<items.length;i++){
+			if(items[i]==null){
+				items[i]=text;
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
@@ -77,12 +105,17 @@ public class ArrayStringList implements StringList {
 
 	@Override
 	public int countElements() {
-		return 0;
+		return 1;
 	}
 
 	@Override
 	public String[] toStringArray() {
 		return new String[0];
+	}
+
+	public static void main(String[] args) {
+		ArrayStringList a = new ArrayStringList();
+		a.appendString("test");
 	}
 	
 	
