8b131ebd490d67eee04b7c83039e5a4fcd753afb
Emmanuel Ndeh <emmanuelrigobert000@gmail.com>
2026-01-02 18:31:24 +0100

    Enhance LinkedStringList functionality: implement previous item tracking, improve append and insert methods, and update countElements and toStringArray for accurate element management.

--- src/miniProject3/LinkedStringList.java
+++ src/miniProject3/LinkedStringList.java
@@ -1,4 +1,9 @@
 package miniProject3;
+
+import codeübung6.Demo;
+
+import java.util.Arrays;
+
 /**
  * A linked list to store strings.
  * 
@@ -23,6 +28,8 @@ public class LinkedStringList implements StringList {
 		 * null, if this is the last element.
 		 */
 		private Item next;
+
+		private Item previous;
 		
 		/**
 		 * Creates an item to store the string.
@@ -30,6 +37,7 @@ public class LinkedStringList implements StringList {
 		 * @param string The string to store.
 		 */
 		public Item(String string) {
+
 			this.string = string;
 		}
 		
@@ -51,6 +59,16 @@ public class LinkedStringList implements StringList {
 		public Item getNext() {
 			return next;
 		}
+
+		/**
+		 * Returns the previous item in the list or
+		 * null, if this is the first item.
+		 *
+		 * @return The next item.
+		 */
+		public Item getPrevious() {
+			return previous;
+		}
 		
 		/**
 		 * Sets the next item of the list.
@@ -60,6 +78,15 @@ public class LinkedStringList implements StringList {
 		public void setNext(Item next) {
 			this.next = next;
 		}
+
+		/**
+		 * Sets the prev item of the list.
+		 *
+		 * @param previous The value for the previous item.
+		 */
+		public void setPrevious(Item previous) {
+			this.previous = previous;
+		}
 		
 	}
 	
@@ -68,6 +95,7 @@ public class LinkedStringList implements StringList {
 	 * list or null, if the list is empty.
 	 */
 	private Item head;
+	private int counter=0;
 	
 	/**
 	 * Creates a new, empty list.
@@ -87,22 +115,100 @@ public class LinkedStringList implements StringList {
 	@Override
 	public void appendString(String text) {
 		// TODO: implement
+		Item item = new Item(text);
+		if(head==null){
+			head=item;
+			counter++;
+			return;
+		}
+		Item lastItem=lastItem();
+		lastItem.setNext(item);
+		item.setPrevious(lastItem);
+		counter++;
+	}
+	public Item lastItem(){
+		Item oldItem=head;
+		Item currentItem;
+		for(int i=0; i<counter;i++){
+			currentItem=oldItem.next;
+			if(currentItem!=null){
+				oldItem=currentItem;
+			}
+		}
+		return oldItem;
 	}
 
 	@Override
 	public String getStringAt(int i) {
 		// TODO: implement
-		return null;
+		return toStringArray()[i];
 	}
 
 	@Override
 	public void insertStringAt(int i, String text) {
 		// TODO: implement
+		if(i>counter) throw new ArrayIndexOutOfBoundsException("Item does not exist");
+		if(counter==0){
+			head=new Item(text);
+			counter++;
+		}
+		Item old=head;
+		for(int j=0; j<counter;j++){
+			if(j==i){
+				Item currentItem = new Item(text);
+				if(i==0){
+					head=currentItem;
+					currentItem.setNext(old);
+					old.setPrevious(head);
+					counter++;
+					return;
+				}
+				old.getPrevious().setNext(currentItem);
+				currentItem.setPrevious(old.getPrevious());
+				currentItem.setNext(old);
+				old.setPrevious(currentItem);
+				counter++;
+				return;
+			}
+			old=old.getNext();
+		}
 	}
 
 	@Override
 	public void insertStringListAt(int i, StringList list) {
 		// TODO: implement
+		//Convert List to LinkedStringList
+		String[] newItems = list.toStringArray();
+		Item newHead = new Item(newItems[0]);
+		Item newTail=newHead;
+		for(int x=1; x<newItems.length;x++){
+			Item newItem = new Item(newItems[x]);
+			newItem.setPrevious(newTail);
+			newTail.setNext(newItem);
+			newTail=newTail.next;
+		}
+		//Fit new linkedList in old linkedList
+		Item current=head;
+		int numOfNewItems = newItems.length;
+		if(i==0){
+			head=newHead;
+			newTail.setNext(current);
+			current.setPrevious(newTail);
+			counter +=numOfNewItems;
+			return;
+		}
+		for(int x=1;x<counter;x++){
+			current=current.getNext();
+			if(i==x){
+				current.getPrevious().setNext(newHead);
+				newHead.setPrevious(current.getPrevious());
+				newTail.setNext(current);
+				current.setPrevious(newTail);
+				counter +=numOfNewItems;
+				return;
+			}
+		}
+
 	}
 
 	@Override
@@ -120,7 +226,7 @@ public class LinkedStringList implements StringList {
 	@Override
 	public String getLastString() {
 		// TODO: implement
-		return null;
+		return counter>0?this.toStringArray()[this.toStringArray().length]: null;
 	}
 
 	@Override
@@ -137,14 +243,32 @@ public class LinkedStringList implements StringList {
 
 	@Override
 	public int countElements() {
-		return 0;
+		return counter;
 	}
 
 	@Override
 	public String[] toStringArray() {
-		return new String[0];
+		String[] list = new String[counter];
+		Item current = head;
+		for(int i=0; i<counter;i++){
+			list[i]=current.getString();
+			current=current.next;
+		}
+		return list;
 	}
-	
-	
+
+	public static void main(String[] args) {
+		LinkedStringList list = new LinkedStringList();
+		list.appendString("Text");
+		list.appendString("Text2");
+//		System.out.println(Arrays.toString(list.toStringArray()));
+		System.out.println("Before: "+Arrays.toString(list.toStringArray()));
+		list.insertStringAt(0,"inserted");
+		System.out.println("After: "+Arrays.toString(list.toStringArray()));
+		list.insertStringListAt(0,list);
+		System.out.println("After: "+Arrays.toString(list.toStringArray()));
+
+
+	}
 	
 }
